5e4b6c8a02900031c83087a4a4d4e8e871d47470
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2023-11-14 21:16:52 +0000

    [feat] - add prefetch option

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://blog.cubadevops.com',
-    integrations: [mdx(), sitemap(), tailwind()]
+    integrations: [mdx(), sitemap(), tailwind()],
+    prefetch: true
 });
