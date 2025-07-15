1255298807f125bdd24d30968163e794ef54dec1
Carlos Batista <cbatista8a@gmail.com>
2025-07-15 10:51:02 +0200

    feat: adds captcha to contact form to avoid spam

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
-    readonly PUBLIC_SENDGRID_KEY: string;
-    readonly PUBLIC_SENDGRID_LIST: string;
     readonly PUBLIC_CAPTCHA: string;
     // más variables de entorno...
   }
-  
+
   interface ImportMeta {
     readonly env: ImportMetaEnv;
   }
